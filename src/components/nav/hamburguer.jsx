47f579c5976f84a8f90a779a87f67ce14fa6d2f2
Luis Herrera <leggetram@gmail.com>
2023-09-24 19:55:43 -0400

    HamburguerBtnWithReact useState and some poor stylish for mock

--- src/components/nav/hamburguer.jsx
+++ src/components/nav/hamburguer.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import '../../styles/Layout.css'
-function hamburguer() {
+import React, { useState } from 'react';
+import '../../styles/Layout.css';
+
+function Hamburguer() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
 
   return (
     <>
-      <input type="checkbox" id='check'/>
+      <input type="checkbox" id='check' checked={isOpen} onChange={toggleMenu} />
       <label htmlFor="check" className='checkBtn'>
-        --- <br />
-        --- <br />
-        ---
+        {isOpen ? (
+          <>
+            <div>&times;</div>
+            <div>&times;</div>
+            <div>&times;</div>
+          </>
+        ) : (
+          <>
+            <div>---</div>
+            <div>---</div>
+            <div>---</div>
+          </>
+        )}
       </label>
-
     </>
-  )
+  );
 }
 
-export default hamburguer;
+export default Hamburguer;
+
+
+
+
+
+
